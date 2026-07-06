63009d9718b4ee57b792f7f6c19cafe3a1d56e51
localmapsanpancho-pixel <localmapsanpancho@gmail.com>
2026-07-06 17:13:18 -0600

    Descripción de los cambios

--- actualizar 2.ps1
+++ actualizar 2.ps1
@@ -1,1 +1,8 @@
 cd "c:\Users\PC GAMER PRO\OrigenBahiaTienda" ; & "C:\Program Files\Git\cmd\git.exe" add index.html ; & "C:\Program Files\Git\cmd\git.exe" commit -m "Cambio en línea 14 de index.html" ; & "C:\Program Files\Git\cmd\git.exe" push origin main
+
+
+git add img_mp/Tortillas_azul.jpeg
+git commit -m "Agregar imagen Tortillas_azul"
+git push origin main
+
+
